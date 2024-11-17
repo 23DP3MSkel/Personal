05fdb1d8e8dc8895f927ca2c3664529676e7d29b
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-11-17 15:38:59 +0000

    PrintInStars

--- Java_mooch_files/kkads_uzd.java
+++ Java_mooch_files/kkads_uzd.java
@@ -6,20 +6,20 @@ public class kkads_uzd {
 
 private static Scanner scanner = new Scanner(System.in);
 
-    public static int sumOfNumbersInArray(int[] array){
-        int sum = 0;
-        for (int i=0;i<array.length;i++){
-            sum += array[i];
+    
+    public static void printArrayInStars(int[] array){
+        for(int i=0;i<array.length;i++){
+            for(int s=0; s<array[i];s++){
+                System.err.print("*");
+            }
+        System.out.println();
         }
-        System.out.println("the sum is: "+ sum);
-        return sum;
     }
-
     public static void main(String[] args) {
         
         
         int[] numbers = {5, 1, 3, 4, 2};
-        sumOfNumbersInArray(numbers);
+        printArrayInStars(numbers);
 
        
     }
