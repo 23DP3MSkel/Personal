06818f0562fb065f148cd17e92c0c0184f651fcf
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-11-17 15:28:35 +0000

    SumOfArray

--- Java_mooch_files/kkads_uzd.java
+++ Java_mooch_files/kkads_uzd.java
@@ -6,36 +6,20 @@ public class kkads_uzd {
 
 private static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        int[] numbers = new int[5];
-        numbers[0] = 42;
-        numbers[1] = 13;
-        numbers[2] = 12;
-        numbers[3] = 7;
-        numbers[4] = 3;
-
-        System.out.print("Search for? ");
-        int find = Integer.valueOf(scanner.nextLine());
-        int found = 0;
-        for(int i=0;i<numbers.length;i++){
-            if(numbers[i] == find){
-                found = i;
-                System.out.println(find + " is at index "+ found);
-                break;
-            }
-            if(i == numbers.length-1){
-                System.out.println(find +" was not found");
-                break;
-            }
-
+    public static int sumOfNumbersInArray(int[] array){
+        int sum = 0;
+        for (int i=0;i<array.length;i++){
+            sum += array[i];
         }
+        System.out.println("the sum is: "+ sum);
+        return sum;
+    }
 
-
-
-
-
-    
+    public static void main(String[] args) {
         
+        
+        int[] numbers = {5, 1, 3, 4, 2};
+        sumOfNumbersInArray(numbers);
 
        
     }
