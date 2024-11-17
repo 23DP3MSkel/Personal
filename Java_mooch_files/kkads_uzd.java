f54b5e258af7b5745b72f987bd9d6073de170f29
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-11-17 16:14:34 +0000

    login(too long)

--- Java_mooch_files/kkads_uzd.java
+++ Java_mooch_files/kkads_uzd.java
@@ -6,21 +6,31 @@ public class kkads_uzd {
 
 private static Scanner scanner = new Scanner(System.in);
 
-    
-    public static void printArrayInStars(int[] array){
-        for(int i=0;i<array.length;i++){
-            for(int s=0; s<array[i];s++){
-                System.err.print("*");
-            }
-        System.out.println();
-        }
-    }
     public static void main(String[] args) {
+        String[] username = {"alex","emma"};
+        String[] password = {"sunshine","haskell"};
         
+        System.out.print("Enter username ");
+        String log = scanner.nextLine();
         
-        int[] numbers = {5, 1, 3, 4, 2};
-        printArrayInStars(numbers);
+        System.out.print("Enter password ");
+        String login = scanner.nextLine();
+        
 
-       
+        for (int i = 0; i<username.length;i++){
+            if(log.equals(username[i])){
+                if(login.equals(password[i])){
+                    System.out.println("You have successfully logged in!");
+                }
+
+                else{
+                    System.out.println("Incorrect username or password!");
+                }
+                break;
+            }
+            if(i == username.length-1){
+                System.out.println("Incorrect username or password!");
+            }
+        }
     }
 }
