2333456b34c359d58045b7d503cd0ed98454c337
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-11-17 16:51:17 +0000

    LineByLine

--- Java_mooch_files/kkads_uzd.java
+++ Java_mooch_files/kkads_uzd.java
@@ -7,23 +7,21 @@ public class kkads_uzd {
 private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String[] username = {"alex","emma"};
-        String[] password = {"sunshine","haskell"};
         
-        System.out.print("Enter username ");
-        String enteredUsername = scanner.nextLine();
-        
-        System.out.print("Enter password ");
-        String enteredPassword = scanner.nextLine();
-        
-
-        for (int i = 0; i<username.length;i++){
-            if(enteredUsername.equals(username[i]) && enteredPassword.equals(password[i])){
-                System.out.println("You logged in!");
-                return;
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("")){
+                break;
+            }
+            else{
+                System.out.println();
+                String[] split = input.split(" ");
+                for(int i=0; i<split.length; i++){
+                    System.out.println(split[i]);
+                }
+                System.out.println();
             }
         }
 
-        System.out.println("Incorrect username or password");
     }
 }
