2e001b49bd500f89942eca8d17156a91b739939e
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-11-17 20:33:38 +0000

    NameOfTheOldest

--- Java_mooch_files/kkads_uzd.java
+++ Java_mooch_files/kkads_uzd.java
@@ -8,7 +8,9 @@ private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         
-    int oldest = -1;
+        int oldest = 0;
+        String EldestName = "";
+        
         while(true){
             String input = scanner.nextLine();
             if(input.equals("")){
@@ -19,11 +21,14 @@ private static Scanner scanner = new Scanner(System.in);
                 String[] split = input.split(",");
                 int age = Integer.valueOf(split[1]);
                 if (age > oldest){
-                    oldest = age;
+                    //oldest = age;
+                    EldestName = split[0];
                 }
                 }
-            }
-            System.out.println("oldest is "+oldest);
         }
+        
+        System.out.println("oldest is "+ EldestName);
+        
+    }
         
     }
