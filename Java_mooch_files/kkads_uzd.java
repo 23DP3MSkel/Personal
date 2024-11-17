cd20a80eb9ccc01e9ac91dc4998e57a04a1a39d6
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-11-17 22:25:44 +0000

    YourFirstBankTransfer

--- Java_mooch_files/kkads_uzd.java
+++ Java_mooch_files/kkads_uzd.java
@@ -7,22 +7,17 @@ public class kkads_uzd {
 private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Account MatthewsAccount = new Account("Matthews account", 1000);
+        Account MyAccount = new Account("My account", 0);
+
+        MatthewsAccount.withdraw(100);
         
-        Account artosAccount = new Account("Arto's account", 100.00);
-        Account artosSwissAccount = new Account("Arto's account in Switzerland", 1000000.00);
-
-        System.out.println("Initial state");
-        System.out.println(artosAccount);
-        System.out.println(artosSwissAccount);
-
-        artosAccount.withdraw(20);
-        System.out.println("The balance of Arto's account is now: " + artosAccount.balance());
-        artosSwissAccount.deposit(200);
-        System.out.println("The balance of Arto's other account is now: " + artosSwissAccount.balance());
-
-        System.out.println("End state");
-        System.out.println(artosAccount);
-        System.out.println(artosSwissAccount);
+        MyAccount.deposit(100);
+        
+        
+        System.out.println(MatthewsAccount);
+        System.out.println(MyAccount);
+        
         
     }
         
