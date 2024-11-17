983561c118dbd6cf19a5dd03833ad3a1acb2cbd3
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-11-17 15:12:36 +0000

    IndexWasNotFound

--- Java_mooch_files/kkads_uzd.java
+++ Java_mooch_files/kkads_uzd.java
@@ -7,24 +7,34 @@ public class kkads_uzd {
 private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-    int[] numbers = new int[5];
-    for (int i=0; i<5; i++){
-        numbers[i] = Integer.valueOf(scanner.nextLine());
-    }
+        int[] numbers = new int[5];
+        numbers[0] = 42;
+        numbers[1] = 13;
+        numbers[2] = 12;
+        numbers[3] = 7;
+        numbers[4] = 3;
+
+        System.out.print("Search for? ");
+        int find = Integer.valueOf(scanner.nextLine());
+        int found = 0;
+        for(int i=0;i<numbers.length;i++){
+            if(numbers[i] == find){
+                found = i;
+                System.out.println(find + " is at index "+ found);
+                break;
+            }
+            if(i == numbers.length-1){
+                System.out.println(find +" was not found");
+                break;
+            }
+
+        }
+
+
+
+
+
     
-    System.out.println();
-    System.out.println("Which to swap?");
-    int first = Integer.valueOf(scanner.nextLine());
-    int second = Integer.valueOf(scanner.nextLine());
-    int temp = numbers[first];
-    numbers[first] = numbers[second];
-    numbers[second] = temp;
-    System.out.println("");
-
-    for (int i=0; i<5;i++){
-        System.out.println(numbers[i]);
-    }
-
         
 
        
