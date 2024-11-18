fe1af1de2f9d45c2f345cb2abbd426d8b22b5a72
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-11-18 19:57:01 +0000

    Statistics

--- Java_mooch_files/kkads_uzd.java
+++ Java_mooch_files/kkads_uzd.java
@@ -8,15 +8,30 @@ private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         
-        DecreasingCounter counter = new DecreasingCounter(100);
+        Statistics allNumbers = new Statistics();
+        Statistics evenNumbers = new Statistics();
+        Statistics oddNumbers = new Statistics();
 
-        counter.printValue();
 
-        counter.reset();
-        counter.printValue();
+        System.out.println("enter the numbers:");
 
-        counter.decrement();
-        counter.printValue();
+
+        while(input){
+            int input = Integer.valueOf(scanner.nextLine());
+            if(input == -1){
+                break;
+            }
+            allNumbers.addNumber(input);
+            if(input % 2 == 0){
+                evenNumbers.addNumber(input);
+            }
+            else{
+                oddNumbers.addNumber(input);
+            }
+        }
+        System.out.println("Sum: "+ allNumbers.sum());
+        System.out.println("Sum of even numbers: "+ evenNumbers.sum());
+        System.out.println("Sum of odd numbers: "+ oddNumbers.sum());
 
         
         
