fc2bb49e19e496ed543e47fad6e33592f5501b52
Marks Rainers Šķēle <158036727+23DP3MSkel@users.noreply.github.com>
2024-11-24 09:12:38 +0000

    OneMinute

--- Java_mooch_files/Timer.java
+++ Java_mooch_files/Timer.java
@@ -2,7 +2,27 @@ package Java_mooch_files;
 
 public class Timer {    
 
-    
+    private ClockHand seconds;
+    private ClockHand hundredSeconds;
+
+    public Timer(){
+        this.seconds = new ClockHand(60);
+        this.hundredSeconds = new ClockHand(100);
+
+    }
+
+    public void advance(){
+        this.hundredSeconds.advance();
+        if(this.hundredSeconds.value() == 0){
+            this.seconds.advance();
+        }
+
+    }
+
+    public String toString(){
+        return this.seconds + ":" + this.hundredSeconds;
+
+    }    
 
 
 
--- Java_mooch_files/kkads_uzd.java
+++ Java_mooch_files/kkads_uzd.java
@@ -8,34 +8,16 @@ private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        ArrayList<Book> books = new ArrayList<>();
 
-        while(true){
-            System.out.print("Title: ");
-            String title = scanner.nextLine();
-            if (title.equals("")){
-                break;
-            }
-            else{
-                System.out.print("Pages: ");
-                int pages = Integer.valueOf(scanner.nextLine());
+        Timer timer = new Timer();
+        while (true) {
+            System.out.println(timer);
+            timer.advance();
 
-                System.out.print("year: ");
-                int year = Integer.valueOf(scanner.nextLine());
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
 
-                books.add(new Book(title, pages, year));
-            }
-        }
-
-        System.out.print("what do you want to be printed: ");
-        String input = scanner.nextLine();
-
-        for(Book var:books){
-            if (input.equals("everything")){
-                System.out.println(var);
-            }
-            else if (input.equals("name")){
-                System.out.println(var.name());
             }
         }
         
